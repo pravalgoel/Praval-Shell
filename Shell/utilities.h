003bea3ed27617edbf0a182e84eab007f0062a5e
pravalgoel <68332904+pravalgoel@users.noreply.github.com>
2021-03-01 01:09:23 +0530

    Update utilities.h

--- Shell/utilities.h
+++ Shell/utilities.h
@@ -1,13 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+
 #include <memory.h>
 #include <stdbool.h>
-#include <sys/wait.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<signal.h>
+#include<stdlib.h>
+#include <string.h>
+#include"history.h"
+#include<unistd.h>
+#include<sys/wait.h>
+#include"linkedlist.h"
 
 
 char* charAppend(char*, char);
-char* strAppend(char*, char*);
-char* getNewArray();
 bool charInSet(char, char[]);
+char* getNewArray();
+char* strAppend(char*, char*);
+
